9dcc747872844c9ea4c30d15c05ebfe623a9c6d9
Vaibhav Sharma <vaibhav.2022@tecorb.co>
2023-05-01 16:17:55 +0530

    Flutter Liquid Pull To Refresh

---
+++ lib/main.dart
@@ -0,0 +1,137 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
+
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Liquid Pull To Refresh'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<LiquidPullToRefreshState> _refreshIndicatorKey =
+  GlobalKey<LiquidPullToRefreshState>();
+
+  static int refreshNum = 10; // number that changes when refreshed
+  Stream<int> counterStream =
+  Stream<int>.periodic(const Duration(seconds: 3), (x) => refreshNum);
+
+  ScrollController? _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  static final List<String> _items = <String>[
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N'
+  ];
+
+  Future<void> _handleRefresh() {
+    final Completer<void> completer = Completer<void>();
+    Timer(const Duration(seconds: 3), () {
+      completer.complete();
+    });
+    setState(() {
+      refreshNum = Random().nextInt(100);
+    });
+    return completer.future.then<void>((_) {
+      ScaffoldMessenger.of(_scaffoldKey.currentState!.context).showSnackBar(
+        SnackBar(
+          content: const Text('Refresh complete'),
+          action: SnackBarAction(
+            label: 'RETRY',
+            onPressed: () {
+              _refreshIndicatorKey.currentState!.show();
+            },
+          ),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: Stack(
+          children: <Widget>[
+            const Align(
+              alignment: Alignment(-1.0, 0.0),
+              child: Icon(Icons.reorder),
+            ),
+            Align(
+              alignment: const Alignment(-0.3, 0.0),
+              child: Text(widget.title!),
+            ),
+          ],
+        ),
+      ),
+      body: LiquidPullToRefresh(
+        key: _refreshIndicatorKey,
+        onRefresh: _handleRefresh,
+        showChildOpacityTransition: false,
+        child: StreamBuilder<int>(
+          stream: counterStream,
+          builder: (context, snapshot) {
+            return ListView.builder(
+              padding: kMaterialListPadding,
+              itemCount: _items.length,
+              controller: _scrollController,
+              itemBuilder: (BuildContext context, int index) {
+                final String item = _items[index];
+                return ListTile(
+                  isThreeLine: true,
+                  leading: CircleAvatar(child: Text(item)),
+                  title: Text('This item represents $item.'),
+                  subtitle: Text(
+                      'Even more additional list item information appears on line three. ${snapshot.data}'),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
